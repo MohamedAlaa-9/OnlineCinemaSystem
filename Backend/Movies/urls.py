482d00a89c9d8dcad4fc51377fd6f0d4a70d4a6b
youssefbassem42 <youssefbassem42@gmail.com>
2025-05-31 21:42:29 +0300

    changess

--- Backend/Movies/urls.py
+++ Backend/Movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import  Home, MovieDetails
+from .views import  Home, MovieDetails, MovieReviewView
 
 urlpatterns = [
     path('home/', Home.as_view(), name="home"),
     path('<str:name>/', MovieDetails.as_view(), name="movie_detail"),
+    path('<str:name>/reviews/', MovieReviewView.as_view(), name="movie_reviews"),
 ]
--- Backend/Movies/views.py
+++ Backend/Movies/views.py
@@ -33,16 +33,33 @@ class MovieDetails(APIView):
     def post(self, request, name):
         try:
             movie = Movie.objects.get(title=name)
+            serializer = MovieDetailsSerializer(movie, data=request.data)
+        except Movie.DoesNotExist:
+            return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+class MovieReviewView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, name):
+        try:
+            movie = Movie.objects.get(title=name)
+            reviews = Review.objects.filter(movie=movie)
+        except Movie.DoesNotExist:
+            return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, name):
+        try:
+            movie = Movie.objects.get(title=name)
         except Movie.DoesNotExist:
             return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = ReviewSerializer(data=request.data)
-        user = request.user.id
         if serializer.is_valid():
-            review = serializer.save(
-                movie_id=movie.id,
-                user_id=user,
-                id=uuid.uuid4()
-            )
-            return Response({"message": "Review added"}, status=status.HTTP_201_CREATED)
+            review = serializer.save(movie=movie, user=request.user)
+            return Response(ReviewSerializer(review).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
